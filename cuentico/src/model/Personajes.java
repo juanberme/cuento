fcbab3417348712c5e0924301e3a757421d73428
juan camilo bermeo <camilo22601@gmail.com>
2020-04-07 17:36:04 -0500

    tercero

--- cuentico/src/model/Personajes.java
+++ cuentico/src/model/Personajes.java
@@ -1,5 +1,14 @@
 package model;
 
-public class Personajes {
-
+public abstract class Personajes {
+	
+	int posx, posy, tamx, tamy;
+	
+	public Personajes(int posx, int posy, int tamx, int tamy) {
+		this.posx = posx;
+		this.posy = posy;
+		this.tamx = tamx;
+		this.tamx = tamx;
+		this.tamy = tamy;
+	}
 }
