f932677f6221cd5bb68f2a591485edac08b11de8
joctypo <oscargiraldop1999@gmail.com>
2020-04-07 18:27:35 -0500

    quinto

--- cuentico/src/model/Logic.java
+++ cuentico/src/model/Logic.java
@@ -9,14 +9,24 @@ public class Logic {
 	private PApplet app;
 	private String[] info1;
 	private LinkedList<Personajes> animales;
-	//private LinkedList<Casas> casitas;
+	private LinkedList<Casas> casitas;
 	private PImage imagencita;
 	
 	public Logic(PApplet app) {
 		
+		cargar();
 		
 		
 	}
+	
+	public void  cargar() {
+		info1= app.loadStrings("");
+		
+		
+		
+	}
+	
+	
 	
 	
 	
