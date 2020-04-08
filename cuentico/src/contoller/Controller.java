abafe8ba837289e6c3af48c989453eb29f76f54c
juan camilo bermeo <camilo22601@gmail.com>
2020-04-07 23:47:46 -0500

    decimo

--- cuentico/src/contoller/Controller.java
+++ cuentico/src/contoller/Controller.java
@@ -17,16 +17,31 @@ public class Controller {
 	}
 
 	public void mousencima() {
-		// area paja
+		// casa paja
+
+		if (app2.mouseX > 71 && app2.mouseX < 232 && app2.mouseY > 204 && app2.mouseY < 406) {
+
+		}
 		
+		// casa madera
+
+		if (app2.mouseX > 312 && app2.mouseX < 467 && app2.mouseY > 147 && app2.mouseY < 347) {
+
+		}
 		
+		// casa ladrillo
+
+		if (app2.mouseX > 574 && app2.mouseX < 750 && app2.mouseY > 207 && app2.mouseY < 393) {
+
+		}
+
 	}
 
 	public void clikeo() {
 
 		// area paja
 		if (app2.mouseX > 85 && app2.mouseX < 202 && app2.mouseY > 318 && app2.mouseY < 394) {
-			
+
 		}
 		// area madera
 		if (app2.mouseX > 331 && app2.mouseX < 427 && app2.mouseY > 286 && app2.mouseY < 333) {
