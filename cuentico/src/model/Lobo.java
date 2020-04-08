0c79b8176129bd062a417e85369fb79c9b95b188
juan camilo bermeo <camilo22601@gmail.com>
2020-04-08 00:14:19 -0500

    onceaba

--- cuentico/src/model/Lobo.java
+++ cuentico/src/model/Lobo.java
@@ -1,11 +1,16 @@
 package model;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Lobo extends Personajes{
 
-	public Lobo(int posx, int posy, int tamx, int tamy, PApplet app) {
-		super(posx, posy, tamx, tamy, null, null, app);
+	public Lobo(int posx, int posy, int tamx, int tamy, PImage lobito, PImage lobito2, PApplet app) {
+		super(posx, posy, tamx, tamy, lobito, lobito2, app);
+	}
+	
+	public void draw() {
+		appp.image(getImagen1(), getPosx(), getPosy());
 	}
 
 }
